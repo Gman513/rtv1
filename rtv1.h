0122698c9d89e8d8e4ab1cb078075639c637439d
Gregory HAVENGA <ghavenga@e5r5p2.jnb.42.fr>
2016-07-11 11:15:55 +0200

    shape structres established

--- rtv1.h
+++ rtv1.h
@@ -6,7 +6,7 @@
 /*   By: ghavenga <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/11 10:44:20 by ghavenga          #+#    #+#             */
-/*   Updated: 2016/07/11 10:54:37 by ghavenga         ###   ########.fr       */
+/*   Updated: 2016/07/11 11:14:58 by ghavenga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,58 @@ typedef struct		s_scene
 {
 	
 }					t_scene;
+
+typedef struct		s_object
+{
+	
+}					t_object;
+
+typedef struct		s_vertex
+{
+	float			x;
+	float			y;
+	float			z;
+}					t_vertex;
+
+typedef struct		s_cylinder
+{
+	t_vertex		apex_o;
+	t_vertex		base_o;
+	float			r;
+}
+
+typedef struct		s_plane
+{
+	t_vertex		tl;
+	t_vertex		tr;
+	t_vertex		bl;
+	t_vertex		br;
+}					t_plane;
+
+typedef struct		s_cube
+{
+	t_vertex		tfl;
+	t_vertex		tfr;
+	t_vertex		tbl;
+	t_vertex		tbr;
+	t_vertex		bfl;
+	t_vertex		bfr;
+	t_vertex		bbl;
+	t_vertex		bbr;
+}					t_cube;
+
+typedef struct		s_sphere
+{
+	t_vertex		origin;
+	float			radius;
+}					t_sphere;
+
+typedef struct		s_cone
+{
+	t_vertex		apex;
+	t_vertex		base_o;
+	float			r;
+}					t_cone;
 
 /*rtv1.c*/
 
