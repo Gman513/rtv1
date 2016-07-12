21422e2187a5e6a189cbf7be539dfdb08ef4a537
Gregory HAVENGA <ghavenga@e5r5p2.jnb.42.fr>
2016-07-12 08:27:07 +0200

    File Reading

--- rtv1.c
+++ rtv1.c
@@ -6,15 +6,45 @@
 /*   By: ghavenga <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/11 10:44:49 by ghavenga          #+#    #+#             */
-/*   Updated: 2016/07/11 11:21:17 by ghavenga         ###   ########.fr       */
+/*   Updated: 2016/07/12 08:09:07 by ghavenga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
 
-int		main(int argc, char *argv)
+int			main(int argc, char *argv)
 {
+	int		fd;
+	t_scene	scene;
+
+
 	if (argc < 1)
+	{
+		fd = open(argv[1]);
+		if (fd = -1)
+			ft_puterror("Could Not Open File");
+		else
+			if (-1 = readscene(fd, scene))
+				ft_puterror("Invalid Scene");
+		//drawscene
+		//showscene
+	}
 	else
 		ft_puterror("No Arguements Given");
+}
+
+int			readscene(int fd, t_scene *scene)
+{
+	char	*buffer;
+	char	**line;
+
+	prepscene(scene);
+	while (get_next_line(&buffer, fd))
+	{
+		line = ft_strsplit(&buffer, '\t');
+		assignobject(&line, scene);
+	}
+	if (scene->camera == NULL || scene->obj_1 == NULL || scene->light_1 == NULL)
+		return (-1);
+	return (0);
 }
--- rtv1.h
+++ rtv1.h
@@ -6,7 +6,7 @@
 /*   By: ghavenga <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/11 10:44:20 by ghavenga          #+#    #+#             */
-/*   Updated: 2016/07/11 11:47:55 by ghavenga         ###   ########.fr       */
+/*   Updated: 2016/07/12 08:22:09 by ghavenga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,36 @@
 # define FOV 45
 # define WINX 800
 # define WINY 600
+# define CXO 400
+# define CYO 300
+
+/*Colours*/
+# define WHITE FFFFFF
+# define BLACK 000000
+# define GRAY 0F0F0F
+# define RED FF0000
+# define GREEN 00FF00
+# define BLUE 0000FF
 
 typedef struct		s_scene
 {
-	t_camera		camera;
-	t_object		obj_1;
+	void			*mlx;
+	void			*win;
+	t_camera		*camera;
+	t_object		*obj_1;
+	t_object		*light_1;
 }					t_scene;
 
 typedef	struct		s_camera;
 {
-	t_vertex		origin;
-	t_plane			screen;
+	t_vertex		*origin;
+	t_plane			*screen;
 }					t_camera;
 
 typedef struct		s_object
 {
 	int				type;
-	void			*shape;
+	void			*info;
 	t_object		*next;
 }					t_object;
 
@@ -46,37 +59,45 @@ typedef struct		s_vertex
 	float			z;
 }					t_vertex;
 
+typedef struct		s_light
+{
+	t_vertex		*origin;
+	int				intensity;
+	int				colour;
+}					t_light;
+
 typedef struct		s_cylinder
 {
-	t_vertex		apex_o;
-	t_vertex		base_o;
+	t_vertex		*apex_o;
+	t_vertex		*base_o;
 	float			r;
+	int				colour;
 }					t_cylinder;
 
 typedef struct		s_plane
 {
-	t_vertex		tl;
-	t_vertex		tr;
-	t_vertex		bl;
-	t_vertex		br;
+	t_vertex		*tl;
+	t_vertex		*tr;
+	t_vertex		*bl;
+	t_vertex		*br;
+	int				colour;
 }					t_plane;
 
 typedef struct		s_cube
 {
-	t_vertex		tfl;
-	t_vertex		tfr;
-	t_vertex		tbl;
-	t_vertex		tbr;
-	t_vertex		bfl;
-	t_vertex		bfr;
-	t_vertex		bbl;
-	t_vertex		bbr;
+	t_vertex		*tl;
+	t_vertex		*tr;
+	t_vertex		*bl;
+	t_vertex		*br;
+	int				depth;
+	int				colour;
 }					t_cube;
 
 typedef struct		s_sphere
 {
 	t_vertex		origin;
 	float			radius;
+	int				colour;
 }					t_sphere;
 
 typedef struct		s_cone
@@ -84,9 +105,10 @@ typedef struct		s_cone
 	t_vertex		apex;
 	t_vertex		base_o;
 	float			r;
+	int				colour;
 }					t_cone;
 
 /*rtv1.c*/
-
+int		readscene(int fd, t_scene *scene);
 
 #endif
